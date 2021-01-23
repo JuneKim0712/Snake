fbaaf0c82d73bea86abb971d7dee8a7b267b46b5
JuneKim0712 <Junwoo.kim0712@gmail.com>
2021-01-22 16:00:41 -0800

    c

--- logic.py
+++ logic.py
@@ -1,5 +1,5 @@
 import pygame
-# import time
+import time
 import random
 import numpy as np
 import threading
@@ -10,13 +10,30 @@ def Food_generate(list1):
         a = list2 == list1
         for i in a:
             if all(i):
-                print(list1, list2)
                 return Food_generate(list1)
         return list2
     return
 
-def next_move(snake, food):
-    pass
+
+def next_move(snake, food, direction):
+    if snake[-1][1] < food[1] and direction[1] != -20 and snake[-1][1]+20 < 500:
+        return 'down'
+    elif snake[-1][1] > food[1] and direction[1] != 20 and snake[-1][1]-20 > -20:
+        return 'up'
+    elif snake[-1][0] < food[0] and direction[0] != -20 and snake[-1][0]+20 < 500:
+        return 'right'
+    elif snake[-1][0] > food[0] and direction[0] != 20 and snake[-1][0]-20 > -20:
+        return 'left'
+    #survival
+    elif direction[1] != 20 and snake[-1][1]-20 > -20:
+        return 'up'
+    elif direction[0] != -20 and snake[-1][0]+20 < 500:
+        return 'right'
+    elif direction[1] != -20 and snake[-1][1] + 20 < 500:
+        return 'down'
+    elif direction[0] != 20 and snake[-1][0]-20 > -20:
+        return 'left'
+
 
 def snake():
     pygame.init()
@@ -25,28 +42,26 @@ def snake():
     WHITE, RED, GREEN = [255, 255, 255], [255, 0, 0], [0, 255, 0]
     OPEN = True
     DIRECTION = np.array([20, 0, 0, 0])
-    SNAKE = np.array([[240, 240, 20, 20]])
+    SNAKE = np.array([[240, 240, 20, 20], [260, 240, 20, 20]])
     FOOD = Food_generate(SNAKE)
-    SCORE = 0
+    SCORE = 1
 
     while OPEN:
+        move=next_move(SNAKE, FOOD, DIRECTION)
+        print(move)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 OPEN = False
                 pass
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and DIRECTION[1] != 20:
-                    DIRECTION = np.array([0, -20, 0, 0])
-                elif event.key == pygame.K_DOWN and DIRECTION[1] != -20:
-                    DIRECTION = np.array([0, 20, 0, 0])
-                elif event.key == pygame.K_LEFT and DIRECTION[0] != 20:
-                    DIRECTION = np.array([-20, 0, 0, 0])
-                elif event.key == pygame.K_RIGHT and DIRECTION[0] != -20:
-                    DIRECTION = np.array([20, 0, 0, 0])
-            pass
-        pass
+        if move == 'up' and DIRECTION[1] != 20:
+            DIRECTION = np.array([0, -20, 0, 0])
+        elif move == 'down' and DIRECTION[1] != -20:
+            DIRECTION = np.array([0, 20, 0, 0])
+        elif move == 'left' and DIRECTION[0] != 20:
+            DIRECTION = np.array([-20, 0, 0, 0])
+        elif move == 'right' and DIRECTION[0] != -20:
+            DIRECTION = np.array([20, 0, 0, 0])
         # snake and foo3
-
         if all(FOOD == SNAKE[-1]):
             SNAKE = np.append(SNAKE[1:], SNAKE[-1] + DIRECTION).reshape((SCORE + 1), 4)
             SCORE += 1
@@ -64,7 +79,7 @@ def snake():
         # snake blocked by its own body
         for i in SNAKE[-1]==SNAKE[:-1]:
             if all(i):
-                print(SNAKE)
+                OPEN=False
         # drawing display
         DISPLAY.fill(WHITE)
         # food
@@ -72,7 +87,7 @@ def snake():
         #snake
         for i in SNAKE: pygame.draw.rect(DISPLAY, GREEN, i)
         pygame.display.update()
-        pygame.time.Clock().tick(5)
+        pygame.time.Clock().tick(20)
         continue
         pygame.quit()
         return SCORE
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import numpy as np
 import random
 
+
 def Food_generate(list1):
-    list2=np.array([random.randrange(0, 481, step=20), random.randrange(0, 481, step=20), 20, 20])
     while True:
-        a=list2==list1
         list2 = np.array([random.randrange(0, 481, step=20), random.randrange(0, 481, step=20), 20, 20])
+        a = list2 == list1
         for i in a:
             if all(i):
+                print(list1, list2)
                 continue
         return list2
+    return
 
-list1=np.array([[60, 80, 20, 20]])
 
-for i in range(1, 10000):
-    print(Food_generate(list1))
-#print(list2[:2])
+list1 = np.array([[60, 80, 20, 20], [40, 40, 20, 20]])
+
+for i in range(1, 1000):
+    a = Food_generate(list1)
+# print(list2[:2])
